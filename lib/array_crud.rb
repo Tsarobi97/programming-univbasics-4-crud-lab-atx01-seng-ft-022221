7ed1dbc03ac4ee028c48517933e9ac25c29f57aa
Robert Obi <robobi97@googlemail.com>
2021-02-09 19:53:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  array = Array.new
 end
 
 def create_an_array
-  
+  ok = ["ok", "ko", "kk", "oo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ok.push("hi")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ok.unshift("hello")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ok.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ok.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  ok[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  ok[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  ok[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+ok[-1] = "SMD"
 end
